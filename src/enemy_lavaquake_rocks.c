86c5eac602f5d0442f84f9daaf967e1b2b040582
Lucas Vinze <lucas.vinze@protonmail.com>
2026-08-27 14:18:30 -0500

    vanilla: C-ify enemy_lavaquake_rocks.c speed tables (sm_rev-cfy.4)
    Replace RomFixedPtr cooldown/eproj-param/Y-speed tables with named
    8- and 16-entry arrays. Use sign16 for the cooldown timer.

--- src/enemy_lavaquake_rocks.c
+++ src/enemy_lavaquake_rocks.c
@@ -6,10 +6,24 @@
 #include "variables.h"
 #include "enemy_ai_canon.h"
 
-#define g_word_A2B520 ((uint16*)RomFixedPtr(0xa2b520))
-#define g_word_A2B530 ((uint16*)RomFixedPtr(0xa2b530))
-#define g_word_A2B550 ((uint16*)RomFixedPtr(0xa2b550))
+enum {
+  kLavaquakeRocksProximity = 0x40,
+  kLavaquakeRocksDelayMask = 0xE,
+  kLavaquakeRocksSpeedMask = 0x1E,
+  kLavaquakeRocksRandomSeed = 17,
+};
 
+static const uint16 kLavaquakeRocksCooldown[8] = {
+  0x10, 0x18, 0x20, 0x28, 0x30, 0x38, 0x40, 0x48,
+};
+static const uint16 kLavaquakeRocksEprojParam[16] = {
+  0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23,
+  0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b,
+};
+static const int16 kLavaquakeRocksYSpeed[16] = {
+  0x60, 0x70, 0x80, 0x90, 0xa0, 0xb0, 0xc0, 0xd0,
+  -0x60, -0x70, -0x80, -0x90, -0xa0, -0xb0, -0xc0, -0xd0,
+};
 
 void LavaquakeRocks_Init(void) {  // 0xA2B570
   Enemy_LavaquakeRocks *E = Get_LavaquakeRocks(cur_enemy_index);
@@ -17,7 +31,7 @@ void LavaquakeRocks_Init(void) {  // 0xA2B570
   E->base.instruction_timer = 1;
   E->base.timer = 0;
   E->lrs_var_A = FUNC16(LavaquakeRocks_1);
-  random_number = 17;
+  random_number = kLavaquakeRocksRandomSeed;
 }
 
 void CallLavaQuakeRocksFunc(uint32 ea) {
@@ -35,23 +49,23 @@ void LavaquakeRocks_Main(void) {  // 0xA2B58F
 }
 
 void LavaquakeRocks_1(void) {  // 0xA2B596
-  if (IsSamusWithinEnemy_X(cur_enemy_index, 0x40)) {
-    if (IsSamusWithinEnemy_Y(cur_enemy_index, 0x40))
+  if (IsSamusWithinEnemy_X(cur_enemy_index, kLavaquakeRocksProximity)) {
+    if (IsSamusWithinEnemy_Y(cur_enemy_index, kLavaquakeRocksProximity))
       Get_LavaquakeRocks(cur_enemy_index)->lrs_var_A = FUNC16(LavaquakeRocks_2);
   }
 }
 
 void LavaquakeRocks_2(void) {  // 0xA2B5B2
-  eproj_unk1995 = g_word_A2B550[(NextRandom() & 0x1E) >> 1];
-  int v1 = (NextRandom() & 0x1E) >> 1;
-  SpawnEprojWithGfx(g_word_A2B530[v1], cur_enemy_index, addr_stru_86BD5A);
+  eproj_unk1995 = kLavaquakeRocksYSpeed[(NextRandom() & kLavaquakeRocksSpeedMask) >> 1];
+  int param_index = (NextRandom() & kLavaquakeRocksSpeedMask) >> 1;
+  SpawnEprojWithGfx(kLavaquakeRocksEprojParam[param_index], cur_enemy_index, addr_stru_86BD5A);
   Enemy_LavaquakeRocks *E = Get_LavaquakeRocks(cur_enemy_index);
   E->lrs_var_A = FUNC16(LavaquakeRocks_3);
-  E->lrs_var_B = g_word_A2B520[(uint16)(NextRandom() & 0xE) >> 1];
+  E->lrs_var_B = kLavaquakeRocksCooldown[(uint16)(NextRandom() & kLavaquakeRocksDelayMask) >> 1];
 }
 
 void LavaquakeRocks_3(void) {  // 0xA2B5EA
   Enemy_LavaquakeRocks *E = Get_LavaquakeRocks(cur_enemy_index);
-  if ((--E->lrs_var_B & 0x8000) != 0)
+  if (sign16(--E->lrs_var_B))
     E->lrs_var_A = FUNC16(LavaquakeRocks_1);
 }
