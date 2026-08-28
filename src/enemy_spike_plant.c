e914441247e8b4c4ebad5cff8fdb3b8d66a80e12
Lucas Vinze <lucas.vinze@protonmail.com>
2026-08-27 21:08:30 -0500

    vanilla: C-ify enemy_spike_plant.c travel table (sm_rev-cfy.5)
    Copy Cacatac travel distances and the 3-entry move-func table from
    A2:9F36/9F42. Leftover: A01B is one byte before SpikeShootingPlant_4.

--- src/enemy_spike_plant.c
+++ src/enemy_spike_plant.c
@@ -6,12 +6,20 @@
 #include "variables.h"
 #include "enemy_ai_canon.h"
 
-#define g_word_A29F36 ((uint16*)RomFixedPtr(0xa29f36))
-#define g_off_A29F42 ((uint16*)RomFixedPtr(0xa29f42))
+enum {
+  kSfx2_SpikeShootingPlantSpit = 0x34,
+  kSpikeShootingPlantShootChance = 3,
+};
 
+static const uint16 kSpikeShootingPlantTravel[6] = {
+  0x0010, 0x0040, 0x0050, 0x0060, 0x0070, 0x0080,
+};
+static const uint16 kSpikeShootingPlantMoveFuncs[3] = {
+  0x9FBA, 0x9FEC, 0xA01B,
+};
 
 const uint16 *SpikeShootingPlant_Instr_9F2A(uint16 k, const uint16 *jp) {  // 0xA29F2A
-  QueueSfx2_Max6(0x34);
+  QueueSfx2_Max6(kSfx2_SpikeShootingPlantSpit);
   return jp;
 }
 
@@ -22,17 +30,17 @@ void SpikeShootingPlant_Init(void) {  // 0xA29F48
     SpikeShootingPlant_5();
   else
     SpikeShootingPlant_7();
-  uint16 sspt_parameter_1_low = LOBYTE(E->sspt_parameter_1);
-  E->sspt_var_E = sspt_parameter_1_low;
-  E->sspt_var_F = g_off_A29F42[sspt_parameter_1_low];
-  int v2 = LOBYTE(E->sspt_parameter_2);
-  E->sspt_var_01 = g_word_A29F36[v2] + E->base.x_pos;
-  E->sspt_var_00 = E->base.x_pos - g_word_A29F36[v2];
-  int v3 = (8 * HIBYTE(E->sspt_parameter_2)) >> 1;
-  E->sspt_var_B = kCommonEnemySpeeds_Linear[v3];
-  E->sspt_var_A = kCommonEnemySpeeds_Linear[v3 + 1];
-  E->sspt_var_D = kCommonEnemySpeeds_Linear[v3 + 2];
-  E->sspt_var_C = kCommonEnemySpeeds_Linear[v3 + 3];
+  uint16 dir = LOBYTE(E->sspt_parameter_1);
+  E->sspt_var_E = dir;
+  E->sspt_var_F = kSpikeShootingPlantMoveFuncs[dir];
+  int travel = LOBYTE(E->sspt_parameter_2);
+  E->sspt_var_01 = kSpikeShootingPlantTravel[travel] + E->base.x_pos;
+  E->sspt_var_00 = E->base.x_pos - kSpikeShootingPlantTravel[travel];
+  int speed = (8 * HIBYTE(E->sspt_parameter_2)) >> 1;
+  E->sspt_var_B = kCommonEnemySpeeds_Linear[speed];
+  E->sspt_var_A = kCommonEnemySpeeds_Linear[speed + 1];
+  E->sspt_var_D = kCommonEnemySpeeds_Linear[speed + 2];
+  E->sspt_var_C = kCommonEnemySpeeds_Linear[speed + 3];
 }
 
 void SpikeShootingPlant_Main(void) {  // 0xA29FB3
@@ -64,7 +72,7 @@ void SpikeShootingPlant_4(void) {  // 0xA2A01C
 
   NextRandom();
   Enemy_SpikeShootingPlant *E = Get_SpikeShootingPlant(cur_enemy_index);
-  if (sign16((uint8)(LOBYTE(E->base.frame_counter) + random_number) - 3)) {
+  if (sign16((uint8)(LOBYTE(E->base.frame_counter) + random_number) - kSpikeShootingPlantShootChance)) {
     E->sspt_var_F = FUNC16(nullsub_182);
     if (HIBYTE(E->sspt_parameter_1))
       SpikeShootingPlant_6();
