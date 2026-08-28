dc950f66f989d59dd375f38f009e52209001ccfd
Lucas Vinze <lucas.vinze@protonmail.com>
2026-08-27 21:08:30 -0500

    vanilla: C-ify enemy_ripper.c ilist magics (sm_rev-cfy.5)
    Replace signed ilist/spritemap immediates with addr_kRipper_* and the
    shared frozen Gripper spritemaps. No ROM table copy.

--- src/enemy_ripper.c
+++ src/enemy_ripper.c
@@ -6,43 +6,41 @@
 #include "variables.h"
 #include "enemy_ai_canon.h"
 
-
 void JetPowerRipper_Init(void) {  // 0xA2E318
-  uint16 v0 = addr_kRipper_Ilist_E2E0;
+  uint16 ilist = addr_kRipper_Ilist_E2E0;
   Enemy_JetPowerRipper *E = Get_JetPowerRipper(cur_enemy_index);
   if (E->jprr_parameter_2)
-    v0 = addr_kRipper_Ilist_E2F4;
-  E->base.current_instruction = v0;
-  uint16 v2 = 8 * E->jprr_parameter_1;
-  E->jprr_var_E = v2;
-  int v3 = v2 >> 1;
-  uint16 v4;
+    ilist = addr_kRipper_Ilist_E2F4;
+  E->base.current_instruction = ilist;
+  uint16 speed_idx = 8 * E->jprr_parameter_1;
+  E->jprr_var_E = speed_idx;
+  int v = speed_idx >> 1;
+  uint16 subspeed;
   if (E->jprr_parameter_2) {
-    E->jprr_var_D = kCommonEnemySpeeds_Linear[v3];
-    v4 = kCommonEnemySpeeds_Linear[v3 + 1];
+    E->jprr_var_D = kCommonEnemySpeeds_Linear[v];
+    subspeed = kCommonEnemySpeeds_Linear[v + 1];
   } else {
-    E->jprr_var_D = kCommonEnemySpeeds_Linear[v3 + 2];
-    v4 = kCommonEnemySpeeds_Linear[v3 + 3];
+    E->jprr_var_D = kCommonEnemySpeeds_Linear[v + 2];
+    subspeed = kCommonEnemySpeeds_Linear[v + 3];
   }
-  E->jprr_var_C = v4;
+  E->jprr_var_C = subspeed;
 }
 
 void JetPowerRipper_Main(void) {  // 0xA2E353
-  int16 v3;
-
   Enemy_JetPowerRipper *E = Get_JetPowerRipper(cur_enemy_index);
   if (Enemy_MoveRight_IgnoreSlopes(cur_enemy_index, __PAIR32__(E->jprr_var_D, E->jprr_var_C))) {
-    int v2 = E->jprr_var_E >> 1;
-    if ((E->jprr_var_D & 0x8000) == 0) {
-      E->jprr_var_D = kCommonEnemySpeeds_Linear[v2 + 2];
-      E->jprr_var_C = kCommonEnemySpeeds_Linear[v2 + 3];
-      v3 = -7456;
+    int v = E->jprr_var_E >> 1;
+    uint16 ilist;
+    if (!sign16(E->jprr_var_D)) {
+      E->jprr_var_D = kCommonEnemySpeeds_Linear[v + 2];
+      E->jprr_var_C = kCommonEnemySpeeds_Linear[v + 3];
+      ilist = addr_kRipper_Ilist_E2E0;
     } else {
-      E->jprr_var_D = kCommonEnemySpeeds_Linear[v2];
-      E->jprr_var_C = kCommonEnemySpeeds_Linear[v2 + 1];
-      v3 = -7436;
+      E->jprr_var_D = kCommonEnemySpeeds_Linear[v];
+      E->jprr_var_C = kCommonEnemySpeeds_Linear[v + 1];
+      ilist = addr_kRipper_Ilist_E2F4;
     }
-    E->base.current_instruction = v3;
+    E->base.current_instruction = ilist;
     E->base.instruction_timer = 1;
     E->base.timer = 0;
   }
@@ -53,54 +51,51 @@ void Ripper_Func_1(void) {  // 0xA2E3A0
 }
 
 void JetPowerRipper_Shot(void) {  // 0xA2E3A9
-  int16 v1;
-
   NormalEnemyShotAi();
   Enemy_JetPowerRipper *E = Get_JetPowerRipper(cur_enemy_index);
   if (E->base.frozen_timer) {
-    v1 = -7105;
-    if ((E->jprr_var_D & 0x8000) == 0)
-      v1 = -7093;
-    E->base.spritemap_pointer = v1;
+    uint16 spritemap = addr_kGripper_Sprmap_E43F;
+    if (!sign16(E->jprr_var_D))
+      spritemap = addr_kGripper_Sprmap_E44B;
+    E->base.spritemap_pointer = spritemap;
   }
 }
 
 void Ripper_Init(void) {  // 0xA2E49F
-  uint16 v0 = addr_kRipper_Ilist_E477;
+  uint16 ilist = addr_kRipper_Ilist_E477;
   Enemy_Ripper *E = Get_Ripper(cur_enemy_index);
   if (!E->ripper_parameter_2)
-    v0 = addr_kRipper_Ilist_E48B;
-  E->base.current_instruction = v0;
-  uint16 v2 = 8 * E->ripper_parameter_1;
-  E->ripper_var_E = v2;
-  int v3 = v2 >> 1;
-  uint16 v4;
+    ilist = addr_kRipper_Ilist_E48B;
+  E->base.current_instruction = ilist;
+  uint16 speed_idx = 8 * E->ripper_parameter_1;
+  E->ripper_var_E = speed_idx;
+  int v = speed_idx >> 1;
+  uint16 subspeed;
   if (E->ripper_parameter_2) {
-    E->ripper_var_D = kCommonEnemySpeeds_Linear[v3];
-    v4 = kCommonEnemySpeeds_Linear[v3 + 1];
+    E->ripper_var_D = kCommonEnemySpeeds_Linear[v];
+    subspeed = kCommonEnemySpeeds_Linear[v + 1];
   } else {
-    E->ripper_var_D = kCommonEnemySpeeds_Linear[v3 + 2];
-    v4 = kCommonEnemySpeeds_Linear[v3 + 3];
+    E->ripper_var_D = kCommonEnemySpeeds_Linear[v + 2];
+    subspeed = kCommonEnemySpeeds_Linear[v + 3];
   }
-  E->ripper_var_C = v4;
+  E->ripper_var_C = subspeed;
 }
 
 void Ripper_Main(void) {  // 0xA2E4DA
-  int16 v3;
-
   Enemy_Ripper *E = Get_Ripper(cur_enemy_index);
   if (Enemy_MoveRight_IgnoreSlopes(cur_enemy_index, __PAIR32__(E->ripper_var_D, E->ripper_var_C))) {
-    int v2 = E->ripper_var_E >> 1;
-    if ((E->ripper_var_D & 0x8000) == 0) {
-      E->ripper_var_D = kCommonEnemySpeeds_Linear[v2 + 2];
-      E->ripper_var_C = kCommonEnemySpeeds_Linear[v2 + 3];
-      v3 = -7029;
+    int v = E->ripper_var_E >> 1;
+    uint16 ilist;
+    if (!sign16(E->ripper_var_D)) {
+      E->ripper_var_D = kCommonEnemySpeeds_Linear[v + 2];
+      E->ripper_var_C = kCommonEnemySpeeds_Linear[v + 3];
+      ilist = addr_kRipper_Ilist_E48B;
     } else {
-      E->ripper_var_D = kCommonEnemySpeeds_Linear[v2];
-      E->ripper_var_C = kCommonEnemySpeeds_Linear[v2 + 1];
-      v3 = -7049;
+      E->ripper_var_D = kCommonEnemySpeeds_Linear[v];
+      E->ripper_var_C = kCommonEnemySpeeds_Linear[v + 1];
+      ilist = addr_kRipper_Ilist_E477;
     }
-    E->base.current_instruction = v3;
+    E->base.current_instruction = ilist;
     E->base.instruction_timer = 1;
     E->base.timer = 0;
   }
