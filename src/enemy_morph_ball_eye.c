a1692003b86d97262fee5fb469603c7af7741582
Lucas Vinze <lucas.vinze@protonmail.com>
2026-08-27 21:08:30 -0500

    vanilla: C-ify enemy_morph_ball_eye.c mount tables (sm_rev-cfy.5)
    Replace RomFixedPtr mount ilists and unnamed proximity scalars with
    named 4-entry tables. Leftover: 7E:9100 HDMA overlay.

--- src/enemy_morph_ball_eye.c
+++ src/enemy_morph_ball_eye.c
@@ -7,14 +7,28 @@
 #include "enemy_types.h"
 #include "enemy_ai_canon.h"
 
-#define g_off_A890DA ((uint16*)RomFixedPtr(0xa890da))
+enum {
+  kCollectedItems_MorphBall = 0x04,
+  kMorphBallEyeParam_Mount = 0x8000,
+  kMorphBallEyeParam_FaceRight = 1,
+  kMorphBallEyeDirMask = 0xF,
+  kMorphBallEyeActivateDelay = 32,
+  kSfx2_MorphBallEyeRay = 0x17,
+  kSfx2_Silence = 0x71,
+};
 
-static const int16 g_word_A890CA[4] = { -8, 8, 0, 0 };
-static const int16 g_word_A890D2[4] = { 0, 0, -8, 8 };
-static const uint16 g_word_A89050 = 0x80;
-static const uint16 g_word_A89052 = 0xb0;
-static const uint16 g_word_A89054 = 0x80;
-static const uint16 g_word_A89056 = 0x80;
+static const int16 kMorphBallEyeMountXOff[4] = { -8, 8, 0, 0 };
+static const int16 kMorphBallEyeMountYOff[4] = { 0, 0, -8, 8 };
+static const uint16 kMorphBallEyeMountIlists[4] = {
+  addr_kMorphBallEye_Ilist_9044,
+  addr_kMorphBallEye_Ilist_9038,
+  addr_kMorphBallEye_Ilist_904A,
+  addr_kMorphBallEye_Ilist_903E,
+};
+static const uint16 kMorphBallEyeActivateXRadius = 0x80;
+static const uint16 kMorphBallEyeDeactivateXRadius = 0xb0;
+static const uint16 kMorphBallEyeActivateYRadius = 0x80;
+static const uint16 kMorphBallEyeDeactivateYRadius = 0x80;
 
 void MorphBallEye_Init(void) {  // 0xA89058
   Enemy_MorphBallEye *E = Get_MorphBallEye(cur_enemy_index);
@@ -22,36 +36,36 @@ void MorphBallEye_Init(void) {  // 0xA89058
   E->base.spritemap_pointer = addr_kSpritemap_Nothing_A8;
   E->base.instruction_timer = 1;
   E->base.timer = 0;
-  if ((E->mbee_parameter_2 & 0x8000) != 0) {
-    int v1 = E->mbee_parameter_2 & 0xF;
-    E->base.x_pos += g_word_A890CA[v1];
-    E->base.y_pos += g_word_A890D2[v1];
+  if (E->mbee_parameter_2 & kMorphBallEyeParam_Mount) {
+    int dir = E->mbee_parameter_2 & kMorphBallEyeDirMask;
+    E->base.x_pos += kMorphBallEyeMountXOff[dir];
+    E->base.y_pos += kMorphBallEyeMountYOff[dir];
     E->mbee_var_F = FUNC16(nullsub_244);
-    E->base.current_instruction = g_off_A890DA[v1];
+    E->base.current_instruction = kMorphBallEyeMountIlists[dir];
     for (int i = 510; i >= 0; i -= 2)
       *(uint16 *)((uint8 *)&g_word_7E9100 + (uint16)i) = 255;
   } else {
     E->mbee_var_F = FUNC16(MorphBallEye_Func_1);
-    if ((E->mbee_parameter_1 & 1) != 0)
-      E->base.current_instruction = addr_stru_A8900E;
+    if (E->mbee_parameter_1 & kMorphBallEyeParam_FaceRight)
+      E->base.current_instruction = addr_kMorphBallEye_Ilist_900E;
     else
-      E->base.current_instruction = addr_stru_A88FFC;
+      E->base.current_instruction = addr_kMorphBallEye_Ilist_8FFC;
   }
 }
 
 void MorphBallEye_Main(void) {  // 0xA890E2
-  if ((collected_items & 4) != 0) {
+  if (collected_items & kCollectedItems_MorphBall) {
     Enemy_MorphBallEye *E = Get_MorphBallEye(cur_enemy_index);
     EnemyRunPreInstr(E->mbee_var_F);
   }
 }
 
 void MorphBallEye_Func_1(uint16 k) {  // 0xA890F1
-  if (IsSamusWithinEnemy_Y(k, g_word_A89054) && IsSamusWithinEnemy_X(k, g_word_A89050)) {
+  if (IsSamusWithinEnemy_Y(k, kMorphBallEyeActivateYRadius) && IsSamusWithinEnemy_X(k, kMorphBallEyeActivateXRadius)) {
     Enemy_MorphBallEye *E = Get_MorphBallEye(k);
-    E->mbee_var_E = 32;
+    E->mbee_var_E = kMorphBallEyeActivateDelay;
     E->base.instruction_timer = 1;
-    if ((E->mbee_parameter_1 & 1) != 0)
+    if (E->mbee_parameter_1 & kMorphBallEyeParam_FaceRight)
       E->base.current_instruction = addr_kMorphBallEye_Ilist_9026;
     else
       E->base.current_instruction = addr_kMorphBallEye_Ilist_9014;
@@ -61,10 +75,10 @@ void MorphBallEye_Func_1(uint16 k) {  // 0xA890F1
 
 void MorphBallEye_Func_2(uint16 k) {  // 0xA8912E
   Enemy_MorphBallEye *E = Get_MorphBallEye(k);
-  bool v2 = E->mbee_var_E == 1;
-  bool v3 = (--E->mbee_var_E & 0x8000) != 0;
-  if (v2 || v3) {
-    QueueSfx2_Max6(0x17);
+  bool expired = E->mbee_var_E == 1;
+  bool underflow = sign16(--E->mbee_var_E);
+  if (expired || underflow) {
+    QueueSfx2_Max6(kSfx2_MorphBallEyeRay);
     SpawnMorphBallEyeBeamHdma();
     E->mbee_var_F = FUNC16(MorphBallEye_Func_3);
     E->mbee_var_D = CalculateAngleFromXY(samus_x_pos - E->base.x_pos, samus_y_pos - E->base.y_pos);
@@ -73,15 +87,15 @@ void MorphBallEye_Func_2(uint16 k) {  // 0xA8912E
 
 void MorphBallEye_Func_3(uint16 k) {  // 0xA89160
   Enemy_MorphBallEye *E = Get_MorphBallEye(k);
-  if (IsSamusWithinEnemy_Y(k, g_word_A89056) && IsSamusWithinEnemy_X(k, g_word_A89052)) {
-    uint16 v3 = CalculateAngleFromXY(samus_x_pos - E->base.x_pos, samus_y_pos - E->base.y_pos);
-    E->mbee_var_D = v3;
-    E->base.current_instruction = ((v3 & 0xF0) >> 2) - 28756;
+  if (IsSamusWithinEnemy_Y(k, kMorphBallEyeDeactivateYRadius) && IsSamusWithinEnemy_X(k, kMorphBallEyeDeactivateXRadius)) {
+    uint16 angle = CalculateAngleFromXY(samus_x_pos - E->base.x_pos, samus_y_pos - E->base.y_pos);
+    E->mbee_var_D = angle;
+    E->base.current_instruction = addr_kMorphBallEye_Ilist_8FAC + ((angle & 0xF0) >> 2);
   } else {
-    QueueSfx2_Max6(0x71);
+    QueueSfx2_Max6(kSfx2_Silence);
     E->mbee_var_C = 0;
-    E->mbee_var_E = 32;
-    if ((E->mbee_parameter_1 & 1) != 0)
+    E->mbee_var_E = kMorphBallEyeActivateDelay;
+    if (E->mbee_parameter_1 & kMorphBallEyeParam_FaceRight)
       E->base.current_instruction = addr_kMorphBallEye_Ilist_9002;
     else
       E->base.current_instruction = addr_kMorphBallEye_Ilist_8FF0;
@@ -92,9 +106,8 @@ void MorphBallEye_Func_3(uint16 k) {  // 0xA89160
 
 void MorphBallEye_Func_4(uint16 k) {  // 0xA891CE
   Enemy_MorphBallEye *E = Get_MorphBallEye(k);
-  bool v2 = E->mbee_var_E == 1;
-  bool v3 = (--E->mbee_var_E & 0x8000) != 0;
-  if (v2 || v3)
+  bool expired = E->mbee_var_E == 1;
+  bool underflow = sign16(--E->mbee_var_E);
+  if (expired || underflow)
     E->mbee_var_F = FUNC16(MorphBallEye_Func_1);
 }
-
